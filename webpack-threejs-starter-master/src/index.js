6525ae980306067eac8fde422e070c5fb0c0bc68
Unknown <ville.sutinen@metropolia.fi>
2019-04-14 20:01:12 +0300

    Orbit controller lisätty

--- webpack-threejs-starter-master/src/index.js
+++ webpack-threejs-starter-master/src/index.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import * as OrbitControls from 'three-orbitcontrols';
+
+
 
 // create a scene
 const scene = new THREE.Scene();
@@ -9,27 +12,43 @@ const material = new THREE.MeshLambertMaterial({ color: 0x0000ab });
 const box = new THREE.Mesh(geometry, material);
 scene.add(box);
 
+
+
+
 // add some light
 const light = new THREE.PointLight(0xffabba);
 light.position.set(10, 10, 35);
 light.intensity = 2;
+const ambiendLight = new THREE.AmbientLight(0xffffff, 1.0);
+scene.add(ambiendLight);
 scene.add(light);
 
 // create a camera and set position
 const camera = new THREE.PerspectiveCamera(75, (window.innerWidth / window.innerHeight), 0.1, 1000);
 camera.position.z = 100;
 
+
+
+
 // create a renderer & add to DOM
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+//using
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.screenSpacePanning = false;
+controls.minDistance = 10;
+controls.maxDistance = 500;
+
+
 // set & start rendering the scene
 const render = () => {
   requestAnimationFrame(render);
-  box.rotation.x += 0.02;
-  box.rotation.y += 0.03;
-  box.rotation.z += 0.05;
+
+  controls.update();
   renderer.render(scene, camera);
 };
 render();
